77ea253ec89bbaba97ec00045326d5ef6cc4f3f1
Anita Nystad <anita@saspon.com>
2019-02-11 19:52:54 +0100

    initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\anita.nystad\OneDrive\Documents\Programmering\Utvikling\DjangoGirls\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
